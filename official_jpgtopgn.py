62d17b730fe34e8c922814036de4e27306c2d296
Yego99 <154710402+Yego99@users.noreply.github.com>
2024-02-14 19:15:55 -0600

    Add files via upload

---
+++ official_jpgtopgn.py
@@ -0,0 +1,18 @@
+#This project will take 2 argument from the command line and turn the pictures in the input folder into png 
+#and put them in the output folder
+
+import sys
+import os
+from PIL import Image
+
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    clean_name = os.path.splitext(filename)[0]
+    img.save(f'{output_folder}{clean_name}.png', 'png')
+    print('all done!')
